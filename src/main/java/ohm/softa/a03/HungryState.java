2d32fd7c2ffb1658aa20b58c9445d87f67682951
friedrichma75987 <friedrichma75987@th-nuernberg.de>
2021-03-31 09:06:23 +0200

    little refactoring, remaining time is now implemented

--- src/main/java/ohm/softa/a03/HungryState.java
+++ src/main/java/ohm/softa/a03/HungryState.java
@@ -9,7 +9,7 @@ public class HungryState extends State {
     State feed(Cat cat) {
         logger.info("You feed the cat...");
 
-        return new DigestingState(cat.getDigest());
+        return new DigestingState(cat.getDigest(), cat.getAwake() - getTime());
     }
 
     @Override
